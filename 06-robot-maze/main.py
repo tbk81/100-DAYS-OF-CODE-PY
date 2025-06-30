b68bab289e013f1b851323fd7d48a1daf665245c
MBP <dev@tbk81.com>
2025-06-30 10:24:12 -0700

    reborgs_world

---
+++ 06-robot-maze/main.py
@@ -0,0 +1,18 @@
+def turnRight():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turnRight()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
