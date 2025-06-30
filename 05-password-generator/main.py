d614ce97e8342a3948b97a845d968b2b55dafb3f
MBP <dev@tbk81.com>
2025-06-30 08:31:52 -0700

    pw finishing

--- 05-password-generator/main.py
+++ 05-password-generator/main.py
@@ -6,20 +6,20 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-in_char= int((input("How many letters would you like in your password?\n")))
+in_char = int((input("How many letters would you like in your password?\n")))
 in_sym = int((input(f"How many symbols would you like?\n")))
 in_num = int((input(f"How many numbers would you like?\n")))
 
 pw = []
+for n in range(1, in_char + 1):
+    # pw.append(letters[random.randint(1, len(letters) + 1)])
+    pw.append(random.choice(letters))
 
-for n in range(in_char):
-    pw.append(letters[random.randint(0, len(letters))])
+for n in range(1, in_sym + 1):
+    pw.append(random.choice(symbols))
 
-for n in range(in_sym):
-    pw.append(numbers[random.randint(0, len(numbers))])
-
-for n in range(in_num):
-    pw.append(symbols[random.randint(0, len(symbols))])
+for n in range(1, in_num + 1):
+    pw.append(random.choice(numbers))
 
 random.shuffle(pw)
 pw = "".join(pw)
