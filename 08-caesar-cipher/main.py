f90a350b953308f99f771de058f804adb01ade37
MBP <dev@tbk81.com>
2025-07-01 10:31:33 -0700

    caesar cipher

---
+++ 08-caesar-cipher/main.py
@@ -0,0 +1,12 @@
+# def life_in_weeks(age):
+#     left = 4680 - (age * 52)
+#     print(f"You have {left} weeks left.")
+#
+#
+# life_in_weeks(44)
+
+def calculate_love_score(name1, name2):
+# your code here
+
+# Call your function with hard coded values
+calculate_love_score("Kanye West", "Kim Kardashian")
