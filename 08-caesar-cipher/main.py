41ccedb34a14c3bd94067a984fba9f2af1592cf3
tbk81 <dev@tbk81.com>
2025-07-04 06:12:18 -0700

    caesar

--- 08-caesar-cipher/main.py
+++ 08-caesar-cipher/main.py
@@ -1,25 +1,27 @@
 from art import logo
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-print(logo)
+# print(logo)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt, 'e' to exit:\n")
 usr_text = input("Type your message:\n").lower()
 usr_shift = int(input("Type the shift number:\n"))
 
+def caesar(dir=direction, text=usr_text, shift=usr_shift):
+    message = ""
 
-def encrypt(text, shift):
-    print(text)
-    alphabet.index(shift)
+    if dir == "encode":
+        print(f"{text}")
+        for c in text:
+            message += alphabet[(alphabet.index(c) + shift) % (len(alphabet))]
+        print(f"Here is your encoded text: {message}")
+    elif dir == "decode":
+        print(f"{text}")
+        for c in text:
+            message += alphabet[(alphabet.index(c) - shift) % (len(alphabet))]
+        print(f"Here is your decoded text: {message}")
+    else:
+        print("Please choose encode or decode")
 
-
-encrypt(usr_text, usr_shift)
-
-# TODO - 1: Create a function called 'encrypt()' that takes 'text' and 'shift' a 2 inputs
-# TODO - 2: Inside the 'encrypt()' function, shift each letter of the text forward in the alphabet by the shift amount
-#  and print the encrypted text.
-# TODO - 3: Call the 'encrypt()' function and pass in the user inputs.
-# TODO - 4: What happens when you shift by 9? Can you fix the code?
+caesar(direction, usr_text, usr_shift)
