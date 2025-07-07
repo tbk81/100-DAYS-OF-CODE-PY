646a919e009819033b83406f3edb312d0eeec37f
MBP <dev@tbk81.com>
2025-07-07 13:12:12 -0700

    silent auction done

--- 09-silent-auction/main.py
+++ 09-silent-auction/main.py
@@ -1,22 +1,26 @@
 from art import logo
+
 print(logo)
 
-def winner(dict):
-    high_bid = {"name": 0}
-    for key in dict:
-        if dict[key] > high_bid["name"]:
-            high_bid["name"] = dict[key]
-    print(high_bid)
+
+# def winner(my_dict):
+#     high_bid = {"name": 0}
+#     for key in my_dict:
+#         if my_dict[key] > high_bid["name"]:
+#             high_bid["name"] = my_dict[key]
+#     print(high_bid)
+
 
 auction_list = {}
 auction_done = False
 while not auction_done:
     name = input("What is your name?: ")
-    bid = int(input("What is your bid?: "))
+    bid = float(input("What is your bid?: "))
     auction_list[name] = bid
     bidders = input("Other bidders, 'yes' or 'no'?: ").lower()
     if bidders == "no":
+        high_bid = max(auction_list, key=auction_list.get)
+        print(f"{high_bid} had the highest bid of ${auction_list[high_bid]}")
         auction_done = True
-
-print(auction_list)
-winner(auction_list)
+    elif bidders == "yes":
+        print("\n"*100)
