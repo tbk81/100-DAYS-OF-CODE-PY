82e1fa6036a6efe8096d4659690bee0f8c11e260
MBP <dev@tbk81.com>
2025-07-07 17:01:27 -0700

    blackjack

--- 11-blackjack-game/main.py
+++ 11-blackjack-game/main.py
@@ -1,1 +1,14 @@
 import random
+
+deck = [11, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 10, 10, 10]
+
+player_deck = []
+comp_deck = []
+
+# turn this into a function
+for i in range(2):
+    player_deck.append(random.choice(deck))
+    comp_deck.append(random.choice(deck))
+
+print(player_deck)
+print(comp_deck)
