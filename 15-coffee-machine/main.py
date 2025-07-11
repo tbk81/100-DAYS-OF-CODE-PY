618b404a61dcada86b5d26da90a8a0d741bc94a0
tbk81 <dev@tbk81.com>
2025-07-11 06:12:26 -0700

    coffee machine fin

--- 15-coffee-machine/main.py
+++ 15-coffee-machine/main.py
@@ -1,8 +1,5 @@
 from menu_file import menu
 
-# print(menu)
-# print(menu["espresso"]['ingredients']['water'])
-# print(menu["espresso"]['cost'])
 
 current_resources = {
     "water": 1000,
@@ -20,7 +17,37 @@ def check_resources(drink):
     for key in menu[drink]['ingredients']:
         if menu[drink]['ingredients'][key] > current_resources[key]:
             print(f"There is not enough {key}")
+            return False
     return True
+
+
+def coin_op():
+    total = float(0)
+    quart = float(input("How many quarters: "))
+    di = float(input("How many dimes: "))
+    nic = float(input("How many nickles: "))
+    pen = float(input("How many pennies: "))
+    total = quart*.25 + di*.1 + nic*.05 + pen*.01
+    return total
+
+
+def transaction_op(money, drink):
+    drink_cost = menu[drink]['cost']
+    if money < drink_cost:
+        print("Not enough money.")
+        return False
+    elif money > drink_cost:
+        print(f"Here is your change: ${money - drink_cost:.2f}")
+        current_resources['money'] += drink_cost
+        return True
+    else:
+        current_resources['money'] += drink_cost
+        return True
+    
+
+def deduct_resources(drink):
+    for key in menu[drink]['ingredients']:
+        current_resources[key] -= menu[drink]['ingredients'][key]
 
 
 machine_on = True
@@ -32,7 +59,8 @@ while machine_on:
         pr_report()
     else:
         if check_resources(usr_choice):
-            print("hello")
+            if transaction_op(coin_op(), usr_choice):
+                deduct_resources(usr_choice)
+                print(f"Enjoy your {usr_choice}!")
         else:
             machine_on = False
-
