cf16878d9a2bbe202224fc040ff76e377b307dbe
MBP <dev@tbk81.com>
2025-07-18 17:04:59 -0700

    turtle race

---
+++ 19-turtle-race/main.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+import random
+
+start_race = False
+screen = Screen()
+screen.setup(width=500, height=400)
+usr_bet = screen.textinput(title="Turtle Race", prompt="Which turtle will win the race? Enter a color:")
+print(usr_bet)
+
+colors = ["red", "green", "blue", "yellow", "orange", "purple"]
+all_turtles = []
+y_pos = -70
+
+for c in colors:
+    tur_obj = Turtle(shape="turtle")
+    tur_obj.color(c)
+    tur_obj.penup()
+    tur_obj.goto(x=-240, y=y_pos)
+    y_pos += 30
+    all_turtles.append(tur_obj)
+
+if usr_bet:
+    start_race = True
+
+while start_race:
+    for turtle in all_turtles:
+        if turtle.xcor() > 215:
+            winning_turtle = turtle.pencolor()
+            start_race = False
+            if winning_turtle == usr_bet:
+                print(f"You win! The winning turtle was {winning_turtle}.")
+            else:
+                print(f"You lose! The winning turtle was {winning_turtle}.")
+
+        rand_dist = random.randint(0, 10)
+        turtle.forward(rand_dist)
+
+
+screen.exitonclick()
--- 19-turtle-race/notes.py
+++ 19-turtle-race/notes.py
@@ -3,10 +3,38 @@ from turtle import Turtle, Screen
 tur_obj = Turtle()
 screen = Screen()
 
+
 def move_forward():
     tur_obj.forward(10)
 
+
+def move_backward():
+    tur_obj.backward(10)
+
+
+def move_left():
+    tur_obj.left(10)
+
+
+def move_right():
+    tur_obj.right(10)
+
+
+def clear_screen():
+    tur_obj.clear()
+    tur_obj.penup()
+    tur_obj.hideturtle()
+    tur_obj.home()
+    tur_obj.pendown()
+    tur_obj.showturtle()
+
+
 screen.listen()
-screen.onkey(fun=move_forward, key="w")  # When passing a func into a func do not inclue ()
+screen.onkey(fun=move_forward, key="w")  # When passing a func into a func do not include ()
+screen.onkey(fun=move_backward, key="s")
+screen.onkey(fun=move_left, key="a")
+screen.onkey(fun=move_right, key="d")
+screen.onkey(fun=clear_screen, key="c")
+
 
 screen.exitonclick()
