1969332055dd629499233df3240ea756cc45355d
tbk81 <dev@tbk81.com>
2025-07-30 05:06:14 -0700

    daily

--- 24-auto-letter-addressor/main.py
+++ 24-auto-letter-addressor/main.py
@@ -7,18 +7,18 @@ Hint1: This method will help you: https://www.w3schools.com/python/ref_file_read
     Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 """
-name_li = []
+
+PLACEHOLDER = "[name]"
+
 with open("Input/Names/invited_names.txt") as f:
-    invited_names = f.readlines()
-    for name in invited_names:
+    names = f.readlines()
+    # print(names)
+
+with open("Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
         strip_name = name.strip()
-        name_li.append(strip_name)
-
-with open("Input/Letters/starting_letter.txt") as start_letter:
-    new_letter = start_letter.read()
-
-
-for name in name_li:
-    new_letter.replace("[name]", name)
-    with open(f"Output/ReadyToSend/{name}_letter.txt", "w") as file:
-        file.write(new_letter)
+        new_letter = letter_contents.replace(PLACEHOLDER, strip_name)
+        # print(new_letter)
+        with open(f"Output/ReadyToSend/letter_for_{strip_name}.txt", "w") as completed_letter:
+            completed_letter.write(new_letter)
