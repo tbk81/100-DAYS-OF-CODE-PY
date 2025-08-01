67776cec7aedb7c2a56a58c9d4e075b0b64dadd8
MBP <dev@tbk81.com>
2025-08-01 16:58:02 -0700

    NATO project

--- 26-NATO-alpha/notes.py
+++ 26-NATO-alpha/notes.py
@@ -13,9 +13,20 @@
 # new_nums = [n*2 for n in range(1, 5)]
 # print(new_nums)
 
-names = ["Beth", "Alex", "Caroline", "Dave", "Eleanor", "Freddie"]
-short_names = [name for name in names if len(name) < 5]
-print(short_names)
-cap_names = [name.upper() for name in names if len(name) > 5]
-print(cap_names)
+# names = ["Beth", "Alex", "Caroline", "Dave", "Eleanor", "Freddie"]
+# short_names = [name for name in names if len(name) < 5]
+# print(short_names)
+# cap_names = [name.upper() for name in names if len(name) > 5]
+# print(cap_names)
+
+# Dictionary Comprehension
+# new_dict = {new_key:new_value for (key,value) in dict.items()}
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result = {word: len(word) for word in sentence.split()}
+print(result)
+
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+weather_f = {}
+
+print(weather_f)
 
