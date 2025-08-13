00397a1477ffb5a78b2c925021f60e4ddad47673
tbk81 <dev@tbk81.com>
2025-08-13 05:59:41 -0700

    flash hole

--- 31-flash-card-capstone/main.py
+++ 31-flash-card-capstone/main.py
@@ -1,2 +1,35 @@
-BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
+import pandas
 
+
+
+BACKGROUND_COLOR = "#B1DDC6"
+FONT = ("Ariel", 40, "italic")
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Flash Hole")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=850, height=576, bg=BACKGROUND_COLOR, highlightthickness=0)
+front_card_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(425, 288, image=front_card_img)
+canvas.grid(column=0, row=0, columnspan=2)
+
+# Buttons
+wrong_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
+wrong_button.grid(column=0, row=1)
+
+correct_image = PhotoImage(file="images/right.png")
+correct_button = Button(image=correct_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
+correct_button.grid(column=1, row=1)
+
+# Canvas text
+port_num = canvas.create_text(400, 150, text="Port Number", font=FONT)
+num = canvas.create_text(400, 263, text="53", font=FONT)
+
+
+window.focus_force()
+window.mainloop()
