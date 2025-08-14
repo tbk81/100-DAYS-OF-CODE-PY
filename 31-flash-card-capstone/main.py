8cab2a7e717a82a02e817d63558c13163a02f681
MBP <dev@tbk81.com>
2025-08-13 18:26:36 -0700

    flash card

--- 31-flash-card-capstone/main.py
+++ 31-flash-card-capstone/main.py
@@ -1,10 +1,34 @@
 from tkinter import *
 import pandas
-
-
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT = ("Ariel", 40, "italic")
+
+df = pandas.read_csv("data/common_ports.csv")
+to_learn = df.to_dict(orient="records")
+
+current_card = {}  # Empty dict to be able to access from other functions
+print(to_learn)
+
+# ---------------------------- Next port button ------------------------------- #
+
+def next_card():
+    global current_card
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(title, text="Port Number", fill="black")
+    canvas.itemconfig(port, text=current_card["port_num"], fill="black")
+    canvas.itemconfig(card_bg, image=front_card_img)
+    window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(title, text="Service", fill="white")
+    canvas.itemconfig(port, text=current_card["service"], fill="white")
+    canvas.itemconfig(card_bg, image=back_card_img)
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -12,24 +36,30 @@ window = Tk()
 window.title("Flash Hole")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+window.after(3000, func=flip_card)
+
 canvas = Canvas(width=850, height=576, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_card_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(425, 288, image=front_card_img)
-canvas.grid(column=0, row=0, columnspan=2)
+back_card_img = PhotoImage(file="images/card_back.png")
+card_bg = canvas.create_image(425, 288, image=front_card_img)
+canvas.grid(column=0, row =0, columnspan=2)
 
 # Buttons
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
+wrong_button = Button(image=wrong_image, highlightthickness=0,
+                      highlightbackground=BACKGROUND_COLOR, command=next_card)
 wrong_button.grid(column=0, row=1)
 
 correct_image = PhotoImage(file="images/right.png")
-correct_button = Button(image=correct_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
+correct_button = Button(image=correct_image, highlightthickness=0,
+                        highlightbackground=BACKGROUND_COLOR, command=next_card)
 correct_button.grid(column=1, row=1)
 
 # Canvas text
-port_num = canvas.create_text(400, 150, text="Port Number", font=FONT)
-num = canvas.create_text(400, 263, text="53", font=FONT)
+title = canvas.create_text(400, 150, text="", fill="black", font=FONT)
+port = canvas.create_text(400, 263, text="", fill="black", font=FONT)
 
 
+next_card()
 window.focus_force()
 window.mainloop()
