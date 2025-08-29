8890daec3c43d9cdb3df12173d8b0863663ee375
MBP <dev@tbk81.com>
2025-08-28 17:01:55 -0700

    rain app

--- 35-rain-alert-app/notes.py
+++ 35-rain-alert-app/notes.py
@@ -1,13 +1,17 @@
 import requests
+import os
 
-API = "5122bf76ae7d608296508c588859cfde"
+API = os.environ.get("OWM_API_KEY")
 URL = "https://api.openweathermap.org/data/2.5/forecast"
-LAT = 32.715736
-LON = -117.161087
+# LAT = 32.715736
+# LON = -117.161087
+LAT = 34.748741
+LON = -92.275101
 
 params = {
     "lat": LAT,
     "lon": LON,
+    "cnt": 4,
     "appid": API
 }
 
@@ -16,8 +20,14 @@ print(response.status_code)
 response.raise_for_status()
 data = response.json()
 # print(data)
-for w in range(len(data["list"])):
-    print(data["list"][w]["weather"][0]["description"])
-    print(data["list"][w]["dt_txt"] + "\n")
+rain = False
+for w in data["list"]:
+    code = w["weather"][0]["id"]
+    if int(code) < 700:
+        rain = True
+        # print(data["list"][w]["weather"][0]["description"])
+        # print(data["list"][w]["dt_txt"] + "\n")
+if rain:
+    print("Bring an umbrella!")
 
 
