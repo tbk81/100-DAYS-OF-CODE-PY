7d060a1a8c1e4e93c1efd226198bce2b85f72b00
MBP <dev@tbk81.com>
2025-08-29 17:03:54 -0700

    stock app

--- 36-stock-news/main.py
+++ 36-stock-news/main.py
@@ -9,6 +9,13 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 STOCK_API = os.environ.get("AV_API_KEY")
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
+
+# STEP 1: Use https://www.alphavantage.co/documentation/#daily
+# When stock price increase/decreases by 5% between yesterday and the day before yesterday, then print("Get News").
+
+# TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries.
+# e.g. [new_value for (key, value) in dictionary.items()]
+
 av_stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
@@ -17,48 +24,56 @@ av_stock_params = {
 
 response = requests.get(STOCK_ENDPOINT, params=av_stock_params)
 data = response.json()
-print(data)
+# print(data)
 
-yesterday_close = data["Time Series (Daily)"]["2025-08-28"]["4. close"]
-print(yesterday_close)
-day_before_close = data["Time Series (Daily)"]["2025-08-27"]["4. close"]
-print(day_before_close)
+daily_close = data["Time Series (Daily)"]
+# print(daily_close)
+# yesterday_close = data["Time Series (Daily)"]["2025-08-28"]["4. close"]
+# print(yesterday_close)
+# day_before_close = data["Time Series (Daily)"]["2025-08-27"]["4. close"]
+# print(day_before_close)
 
-# STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+daily_li = [value for (key, value) in daily_close.items()]
+yesterday_close = daily_li[1]["4. close"]
+print(yesterday_close+"\n")
+# for c in daily_li:
+#     print(c["4. close"])
+# print(daily_li[0]["4. close"])
 
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries.
-# e.g. [new_value for (key, value) in dictionary.items()]
+# TODO 2. - Get the day before yesterday's closing stock price
+day_before_close = daily_li[2]["4. close"]
+print(day_before_close+"\n")
 
-#TODO 2. - Get the day before yesterday's closing stock price
-
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
+# TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
 # Hint: https://www.w3schools.com/python/ref_func_abs.asp
-
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day
+close_diff = abs(float(yesterday_close) - float(day_before_close))
+print(close_diff)
+# TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day
 # before yesterday.
+close_percentage = 1
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+# TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
-    ## STEP 2: https://newsapi.org/
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+# STEP 2: https://newsapi.org/
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+# TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles.
+# TODO 7. - Use Python slice operator to create a list that contains the first 3 articles.
 # Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number.
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+# STEP 3: Use twilio.com/docs/sms/quickstart/python
+# to send a separate message with each article's title and description to your phone number.
 
-#TODO 9. - Send each article as a separate message via Twilio.
+# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+
+# TODO 9. - Send each article as a separate message via Twilio.
 
 
 
-#Optional TODO: Format the message like this:
+# Optional TODO: Format the message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
