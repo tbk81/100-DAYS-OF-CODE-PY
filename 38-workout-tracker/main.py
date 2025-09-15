6e8b7da8fc705131c98a67e4ec3fde97c6e0c446
tbk81 <dev@tbk81.com>
2025-09-15 05:58:58 -0700

    d38 workout tracker

--- 38-workout-tracker/main.py
+++ 38-workout-tracker/main.py
@@ -1,25 +1,30 @@
-from urllib.parse import uses_relative
-
+import os
 import requests
-from charset_normalizer.cli import query_yes_no
 
 APP_ID = os.environ.get("NUTRITIONX_ID")
 APP_API = os.environ.get("NUTRITIONX_API")
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = os.environ.get("SHEETY_ENDPNT")
 
 headers = {
     "Content-Type": 'application/json',
     "x-app-id": APP_ID,
     "x-app-key": APP_API
-}
+    }
 
-usr_input = input("Type your exercise: ")
+# usr_input = input("Type your exercise: ")
 
-params = {
-    "query" = usr_input,
-}
-response = requests.post(url=exercise_endpoint, params=params, headers=headers)
+# params = {
+#     "query": usr_input,
+#     "gender": "male",
+#     "weight_kg": "127",
+#     "height_cm": "187"
+#     }
 
-print(response)
+# nutritionx_response = requests.post(url=exercise_endpoint, json=params, headers=headers)
+# print(nutritionx_response.text)
+
+sheety_response = requests.get(url=sheety_endpoint)
+print(sheety_response.text)
 
