9153e31d995f9d2e06b1ae7b67c54e6739a812af
MBP <dev@tbk81.com>
2025-09-22 17:27:59 -0700

    D39 flight deal finder

--- 39-flight-deal-finder/data_manager.py
+++ 39-flight-deal-finder/data_manager.py
@@ -2,21 +2,21 @@ import os
 import requests
 from requests.auth import HTTPBasicAuth
 
-
 sheety_endpoint = os.environ.get("SHEETY_FLIGHT_ENDPNT")
+sheety_prices_endpoint = os.environ.get("SHEETY_PRICES_ENDPNT")
 
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
+    # This class is responsible for talking to the Google Sheet.
 
     def __init__(self):
-        self._user = os.environ["SHEETY_USERNAME"]
-        self._password = os.environ["SHEETY_PASSWORD"]
+        self._user = os.environ["SHEETY_FLIGHT_UN"]
+        self._password = os.environ["SHEETY_FLIGHT_PW"]
         self._authorization = HTTPBasicAuth(self._user, self._password)
         self.destination_data = {}
 
     def get_destination_data(self):
         # 2. Use the Sheety API to GET all the data in that sheet and print it out.
-        response = requests.get(url=SHEETY_PRICES_ENDPOINT, auth=self._authorization)
+        response = requests.get(url=sheety_endpoint, auth=self._authorization)
         data = response.json()
         self.destination_data = data["prices"]
         # 3. Try importing pretty print and printing the data out again using pprint() to see it formatted.
@@ -33,7 +33,7 @@ class DataManager:
                 }
             }
             response = requests.put(
-                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                url=f"{sheety_prices_endpoint}/{city['id']}",
                 json=new_data,
                 auth=self._authorization
             )
