4d1bf843b86a84a963e0dfc323c6a476a9006b41
tbk81 <dev@tbk81.com>
2025-09-24 05:33:00 -0700

    d39 flight deal finder

--- 39-flight-deal-finder/flight_search.py
+++ 39-flight-deal-finder/flight_search.py
@@ -1,6 +1,38 @@
+import os
+import requests
+
 class FlightSearch:
 
+    def __init__(self):
+        self._api_key = os.environ.get("AMADEUS_API_KEY")
+        self._api_secret = os.environ.get("AMADEUS_API_SECRET")
+        self._token = self._get_new_token()
+        self.amadeus_endpnt = "https://test.api.amadeus.com"
+        self.amadeus_token_endpnt = "/v1/security/oauth2/token"
+
+    def _get_new_token(self):
+        """
+        Generates the authentication token used for accessing the Amadeus API and returns it.
+        This function makes a POST request to the Amadeus token endpoint with the required
+        credentials (API key and API secret) to obtain a new client credentials token.
+        Upon receiving a response, the function updates the FlightSearch instance's token.
+        Returns:
+            str: The new access token obtained from the API response.
+        """
+        amadeus_token_headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            }
+        data = {
+            "grant_type": "client_credentials",
+            "client_id": self._api_key,
+            "client_secret": self._api_secret
+            }
+        token_response = requests.post(self.amadeus_endpnt + self.amadeus_token_endpnt, headers=amadeus_token_headers, data=data)
+        print(f"Your token is {token_response.json()['access_token']}")
+        print(f"Your token expires in {token_response.json()['expires_in']} seconds")
+        return token_response.json()['access token']
+
     def get_destination_code(self, city_name):
-        # Return "TESTING" for now to make sure Sheety is working.
-        code = "TESTING"
-        return code
+        # code = "TESTING"
+        # return code
+        pass
--- 39-flight-deal-finder/main.py
+++ 39-flight-deal-finder/main.py
@@ -44,18 +44,18 @@
 # print(data["prices"])
 
 
-# 4. Pass the data back to the main.py file, so that you can print the data from main.py
+# Passes the data back to the main.py file, so that you can print the data from main.py
 from data_manager import DataManager
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 # print(sheet_data)
 
-#  5. In main.py check if sheet_data contains any values for the "iataCode" key.
+#  Checks if sheet_data contains any values for the "iataCode" key.
 #  If not, then the IATA Codes column is empty in the Google Sheet.
-#  In this case, pass each city name in sheet_data one-by-one
+#  In this case, this passes each city name in sheet_data one-by-one
 #  to the FlightSearch class to get the corresponding IATA code
 #  for that city using the Flight Search API.
-#  You should use the code you get back to update the sheet_data dictionary.
+#  Use the code you get back to update the sheet_data dictionary.
 if sheet_data[0]["iataCode"] == "":
     from flight_search import FlightSearch
     flight_search = FlightSearch()
