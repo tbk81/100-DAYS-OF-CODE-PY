6998a215d94f93c3d7a422b1da5785c6b35f2036
MBP <dev@tbk81.com>
2025-09-25 16:06:24 -0700

    d39 flight deal finder

--- 39-flight-deal-finder/flight_search.py
+++ 39-flight-deal-finder/flight_search.py
@@ -16,7 +16,7 @@ class FlightSearch:
         """
         Generates the authentication token used for accessing the Amadeus API and returns it.
         This function makes a POST request to the Amadeus token endpoint with the required
-        credentials (API key and API secret) to obtain a new client credentials token.
+        credentials (API key and API secret) to get a new client credentials token.
         Upon receiving a response, the function updates the FlightSearch instance's token.
         Returns:
             str: The new access token obtained from the API response.
@@ -77,3 +77,51 @@ class FlightSearch:
             return "Not Found"
 
         return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        """
+        Searches for flight options between two cities on specified departure and return dates
+        using the Amadeus API.
+        Parameters:
+            origin_city_code (str): The IATA code of the departure city.
+            destination_city_code (str): The IATA code of the destination city.
+            from_time (datetime): The departure date.
+            to_time (datetime): The return date.
+        Returns:
+            dict or None: A dictionary containing flight offers data if the query is successful; None
+            if there is an error.
+        The function constructs a query with the flight search parameters and sends a GET request to
+        the API. It handles the response, checking the status code and parsing the JSON data if the
+        request is successful. If the response status code is not 200, it logs an error message and
+        provides a link to the API documentation for status code details.
+        """
+
+        # print(f"Using this token to check_flights() {self._token}")
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time.strftime("%Y-%m-%d"),
+            "returnDate": to_time.strftime("%Y-%m-%d"),
+            "adults": 1,
+            "nonStop": "true",
+            "currencyCode": "GBP",
+            "max": "10",
+        }
+
+        response = requests.get(
+            url=FLIGHT_ENDPOINT,
+            headers=headers,
+            params=query,
+        )
+
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            print("There was a problem with the flight search.\n"
+                  "For details on status codes, check the API documentation:\n"
+                  "https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api"
+                  "-reference")
+            print("Response body:", response.text)
+            return None
+
+        return response.json()
--- 39-flight-deal-finder/main.py
+++ 39-flight-deal-finder/main.py
@@ -1,71 +1,46 @@
-# This file will need to use the DataManager, FlightSearch, FlightData, NotificationManager classes to
-# achieve the program requirements.
 
-# import os
-# import requests
-# from data_manager import DataManager
-
-# AMADEUS_API_KEY = os.environ.get("AMADEUS_API_KEY")
-# AMADEUS_API_SECRET = os.environ.get("AMADEUS_API_SECRET")
-# AMADEUS_TOKEN = os.environ.get("AMADEUS_TOKEN")
-#
-# amadeus_endpnt = "https://test.api.amadeus.com"
-# amadeus_token_endpnt = "/v1/security/oauth2/token"
-# amadeus_city_endpnt = "/reference-data/locations/"
-#
-# amadeus_headers = {
-#     "Authorization": f"Bearer {AMADEUS_TOKEN}"
-# }
-
-# Creating an access token
-# amadeus_token_headers = {
-#     "Content-Type": "application/x-www-form-urlencoded",
-#     }
-# data = {
-#     "grant_type": "client_credentials",
-#     "client_id": AMADEUS_API_KEY,
-#     "client_secret": AMADEUS_API_SECRET
-#     }
-# token_response = requests.post(amadeus_endpnt + amadeus_token_endpnt, headers=amadeus_token_headers, data=data)
-# print(token_response.text)
-
-# city_params = {
-#     "keyword": "PARIS"
-# }
-
-# print(amadeus_endpnt + amadeus_city_endpnt)
-# city_response = requests.post(amadeus_endpnt + amadeus_city_endpnt, headers=amadeus_headers, params=city_params)
-# print(city_response.text)
-# print(city_response)
-
-# data = DataManager()
-# for price in data:
-#     print
-# print(data["prices"])
-
-
-# Passes the data back to the main.py so that you can print the data from main.py
 import time
+from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
+
+# ==================== Set up the Flight Search ====================
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 flight_search = FlightSearch()
-print(sheet_data)
 
-#  Checks if sheet_data contains any values for the "iataCode" key.
-#  If not, then the IATA Codes column is empty in the Google Sheet.
-#  In this case, this passes each city name in sheet_data one-by-one
-#  to the FlightSearch class to get the corresponding IATA code
-#  for that city using the Flight Search API.
-#  Use the code you get back to update the sheet_data dictionary.
+# Set your origin airport
+ORIGIN_CITY_IATA = "LON"
+
+
+# ==================== Update the Airport Codes in Google Sheet ====================
+
 for row in sheet_data:
     if row["iataCode"] == "":
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-        # slowing down requests to avoid the rate limit
+        # slowing down requests to avoid a rate limit
         time.sleep(2)
 print(f"sheet_data:\n {sheet_data}")
 
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
+
+# ==================== Search for Flights ====================
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination['city']}...")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    cheapest_flight = find_cheapest_flight(flights)
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    # Slowing down requests to avoid a rate limit
+    time.sleep(2)
