aa8932496806f64585be32d774c5075718397f49
MBP <dev@tbk81.com>
2025-10-27 15:53:00 -0700

    d47 selenium bot game play

--- 47-game-play-bot/main.py
+++ 47-game-play-bot/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from time import sleep, time
 
@@ -24,26 +25,61 @@ try:
 except NoSuchElementException:
     print("Language selection or consent banner not found")
 
+
+# Find the big cookie to click
+cookie = driver.find_element(by=By.ID, value="bigCookie")
+
+# Get all store items (products 0-17)
+item_ids = [f"product{i}" for i in range(18)]
+
+# Set timers
+wait_time = 5
+timeout = time() + wait_time  # Check for purchases every 5 seconds
+five_min = time() + 60 * 5  # Run for 5 minutes
+
+
 sleep(1)
-cookie = driver.find_element(By.ID, 'bigCookie')
-for n in range(20):
+while True:
     cookie.click()
 
-# side_note = driver.find_element(By.XPATH, '//*[@id="notes"]/div[5]')
-# side_note.click()
+    # Every 5 seconds, try to buy the most expensive item we can afford
+    if time() > timeout:
+        try:
+            # Get current cookie count
+            cookies_element = driver.find_element(by=By.ID, value="cookies")
+            cookie_text = cookies_element.text
+            # Extract number from text like "123 cookies"
+            cookie_count = int(cookie_text.split()[0].replace(",", ""))
 
+            # Find all available products in the store
+            products = driver.find_elements(by=By.CSS_SELECTOR, value="div[id^='product']")
 
-products = driver.find_elements(by=By.CSS_SELECTOR, value="div[id^='product']")
-# products[0].click()
-best_item = None
-for product in reversed(products):  # Start from most expensive (bottom of list)
-    # Check if item is available and affordable (enabled class)
-    if "enabled" in product.get_attribute("class"):
-        best_item = product
+            # Find the most expensive item we can afford
+            best_item = None
+            for product in reversed(products):  # Start from most expensive (bottom of list)
+                # Check if item is available and affordable (enabled class)
+                if "enabled" in product.get_attribute("class"):
+                    best_item = product
+                    break
+
+            # Buy the best item if found
+            if best_item:
+                best_item.click()
+                print(f"Bought item: {best_item.get_attribute('id')}")
+
+        except (NoSuchElementException, ValueError):
+            print("Couldn't find cookie count or items")
+
+        # Reset timer
+        timeout = time() + wait_time
+
+    # Stop after 5 minutes
+    if time() > five_min:
+        try:
+            cookies_element = driver.find_element(by=By.ID, value="cookies")
+            print(f"Final result: {cookies_element.text}")
+        except NoSuchElementException:
+            print("Couldn't get final cookie count")
         break
-
-# Buy the best item if found
-if best_item:
-    best_item.click()
 
 
