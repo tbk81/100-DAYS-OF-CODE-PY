26f26a43aaf0535781e243bf666ab49048183de7
MBP <dev@tbk81.com>
2025-10-27 15:53:13 -0700

    d47 selenium bot game play

--- 47-game-play-bot/main.py
+++ 47-game-play-bot/main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
 from time import sleep, time
 
 # Set up web driver
