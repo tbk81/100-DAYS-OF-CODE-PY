9d6c62005de495a47e26ebaedebf9fb2bba0d083
MBP <dev@tbk81.com>
2025-10-31 14:26:06 -0700

    53-data-entry-capstone

--- 53-data-entry-capstone/main.py
+++ 53-data-entry-capstone/main.py
@@ -20,7 +20,28 @@ with open('website.html') as f:
     data = f.read()
 soup = BeautifulSoup(data, 'html.parser')
 
-addresses = soup.find_all(name='address')
-for addr in addresses:
-    print(addr.text.strip().replace("| ", ",").split(",")[1:]) ##
+# Find and list addresses
+# addresses = soup.find_all(name='address')
+# addr_li = [addr.get_text().replace(" | ", " ").strip() for addr in addresses]
+
+# Find and list addresses
+# prices = soup.select(".StyledPropertyCardDataWrapper span")
+# price_li = [price.get_text().replace("/mo", "").split("+")[0] for price in prices]
+
+# Find and link all links
+links = soup.select(".StyledPropertyCardDataWrapper a")
+link_li = [link['href'] for link in links]
+print(link_li)
+
+
+
+# ==================================== TESTING AND DEV ==================================== #
+
+# addresses = soup.select(".StyledPropertyCardDataWrapper address")
+# for addr in addresses:
+    # print(addr.text.strip())
+    # print(addr.get_text().strip().replace("| ", ",").split(",")[1:])
+    # print(addr.get_text().replace(" | ", " ").strip())
+# li = [addr.get_text().strip().replace("| ", ",").split(",")[1:] for addr in addresses]
+
 
