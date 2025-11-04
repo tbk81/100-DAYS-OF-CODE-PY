78fa6dd904a35411cae80c71f94a34b00c1f0fa7
MBP <dev@tbk81.com>
2025-11-03 18:22:09 -0800

    53-data-entry-capstone

--- 53-data-entry-capstone/main.py
+++ 53-data-entry-capstone/main.py
@@ -1,12 +1,22 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-
+from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
 
 
 GOOGLE_FORM = os.environ.get("GOOGLE_FORM_D53")
-
 zillow_endpoint = "https://appbrewery.github.io/Zillow-Clone/"
+
+# Chrome and driver setup
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+
 
 def write_site(site):
     response = requests.get(site)
@@ -32,7 +42,15 @@ price_li = [price.get_text().replace("/mo", "").split("+")[0] for price in price
 links = soup.select(".StyledPropertyCardDataWrapper a")
 link_li = [link['href'] for link in links]
 
+# Log into website
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(GOOGLE_FORM)
 
+# Log into website
+wait = WebDriverWait(driver, 2)
+
+pw_input = wait.until(ec.element_to_be_clickable((By.CLASS_NAME, "whsOnd zHQkBf")))
+pw_input.send_keys(addr_li[0])
 
 
 # ==================================== TESTING AND DEV ==================================== #
