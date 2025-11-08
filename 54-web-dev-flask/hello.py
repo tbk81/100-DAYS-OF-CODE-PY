fb779fbcbf0d45bdcc9388a5e6d1e6256d1ff365
MBP <dev@tbk81.com>
2025-11-07 16:13:10 -0800

    54-web-dev-flask

--- 54-web-dev-flask/hello.py
+++ 54-web-dev-flask/hello.py
@@ -2,8 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+print(__name__)
+
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 
+# Used to run a script from the file
+if __main__ == "__main__":
+    app.run()
