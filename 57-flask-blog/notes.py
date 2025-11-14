5fb15c19f262b48ec61ba16ef5cc54fed6c591c2
arch <accounts@tbk81.com>
2025-11-14 14:57:46 -0800

    d57 flask blog

--- 57-flask-blog/notes.py
+++ 57-flask-blog/notes.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 from datetime import datetime
 import random
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home()
+def home():
     rando_num = random.randint(0, 100)
     current_year = datetime.now().year
     return render_template('index.html',
@@ -15,7 +16,19 @@ def home()
 
 
 @app.route("/guess/<name>")
-def gender_age():
+def gender_age(name):
+    gender_endpoint = f"https://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_endpoint).json()
+    gender = gender_response['gender']
+    age_endpoint = f"https://api.agify.io?name={name}"
+    age_response = requests.get(age_endpoint).json()
+    age = age_response['age']
+    return render_template('index.html', name=name.title(), gender=gender, age=age)
+
+
+
+
+
 
 
 if __name__ == "__main__":
---
+++ 57-flask-blog/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from datetime import datetime
+import requests
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    rando_num = random.randint(0, 100)
+    current_year = datetime.now().year
+    return render_template('index.html',
+                           num=rando_num,
+                           year=current_year)
+
+
+@app.route("/blog/<num>")
+def get_blog(num):
+    blog_url = " https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url).json()
+    all_posts = response.json()
+    return render_template('blog.html', posts=all_posts)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
