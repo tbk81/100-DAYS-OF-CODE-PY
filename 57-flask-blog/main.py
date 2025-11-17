62d4f255d2fa83db0ccc2585c0034aebc086dd95
MBP <dev@tbk81.com>
2025-11-17 13:47:25 -0800

    d57 flask blog

--- 57-flask-blog/main.py
+++ 57-flask-blog/main.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
+
+headers = {
+    "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+    }
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url, headers=headers, timeout=10)
+    all_posts = response.json()
+    return render_template("index2.html", posts=all_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- 57-flask-blog/server.py
+++ 57-flask-blog/server.py
@@ -17,7 +17,7 @@ def home():
 @app.route("/blog/<num>")
 def get_blog(num):
     blog_url = " https://api.npoint.io/c790b4d5cab58020d391"
-    response = requests.get(blog_url).json()
+    response = requests.get(blog_url)
     all_posts = response.json()
     return render_template('blog.html', posts=all_posts)
 
