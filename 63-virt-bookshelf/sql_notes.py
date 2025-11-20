4eafe4451fda181131409849ec4ccc979d5da924
MBP <dev@tbk81.com>
2025-11-19 18:14:18 -0800

    d63 virtual bookshelf

--- 63-virt-bookshelf/sql_notes.py
+++ 63-virt-bookshelf/sql_notes.py
@@ -28,7 +28,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
 db.init_app(app)
 
 class Book(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)  # When creating new records, the primary key
+    # fields is optional. The id field will be auto-generated.
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     author: Mapped[str] = mapped_column(String(250), nullable=False)
     rating: Mapped[float] = mapped_column(Float, nullable=False)
@@ -41,8 +42,45 @@ class Book(db.Model):
 # with app.app_context():
 #     db.create_all()
 
+# with app.app_context():
+#     new_book = Book(id=1, title="Book 1", author="Author 1", rating=5.3)
+#     db.session.add(new_book)
+#     db.session.commit()
+
 with app.app_context():
-    new_book = Book(id=1, title="Book 1", author="Author 1", rating=5.3)
+    new_book = Book(title="Book 2", author="Author 2", rating=8.9)
     db.session.add(new_book)
     db.session.commit()
+
+# Read all records
+with app.app_context():
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars()
+
+# Read A Particular Record By Query
+with app.app_context():
+    book = db.session.execute(db.select(Book).where(Book.title == "Book 1")).scalar()
+
+# Update A Particular Record By Query
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.title == "Book 1")).scalar()
+    book_to_update.title = "Book 1: The Full Title"
+    db.session.commit()
+
+# Update A Record By PRIMARY KEY
+book_id = 1
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_update = db.get_or_404(Book, book_id)
+    book_to_update.title = "Book 1: The Full Title"
+    db.session.commit()
+
+# Delete A Particular Record By PRIMARY KEY
+book_id = 1
+with app.app_context():
+    book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+
 
