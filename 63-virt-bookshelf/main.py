8b94a8ecff9d0c529f8c1337ca127ea0bff95055
MBP <dev@tbk81.com>
2025-11-21 11:07:54 -0800

    d63 virtual bookshelf

--- 63-virt-bookshelf/main.py
+++ 63-virt-bookshelf/main.py
@@ -50,6 +50,32 @@ def add():
         return redirect(url_for('home'))
     return render_template('add.html')
 
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        #UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit.html", book=book_selected)
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+
+    # DELETE A RECORD BY ID
+    book_to_delete = db.get_or_404(Book, book_id)
+    # Alternative way to select the book to delete.
+    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
