f1b605f2e8a563572b1ef061d320c793ff4f2005
MBP <dev@tbk81.com>
2025-11-24 15:04:14 -0800

    d64 movie list

--- 64-top10-movies-website/main.py
+++ 64-top10-movies-website/main.py
@@ -8,28 +8,72 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
+
 # CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie-list.db"
+db = SQLAlchemy(model_class=Base)
+# initialize the app with the extension
+db.init_app(app)
 
 
 # CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)  # When creating new records, the primary key
+    # fields is optional. The id field will be auto-generated.
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[str] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
+    # This will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+with app.app_context():
+    db.create_all()
+
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+
+# with app.app_context():
+#     db.session.add(new_movie)
+#     db.session.commit()
+
+# second_movie = Movie(
+#     title="Avatar The Way of Water",
+#     year=2022,
+#     description="Set more than a decade after the events of the first film, learn the story of the
+#     Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they
+#     go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#     rating=7.3,
+#     ranking=9,
+#     review="I liked the water.",
+#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+# )
+
+# with app.app_context():
+#     db.session.add(second_movie)
+#     db.session.commit()
 
 @app.route("/")
 def home():
