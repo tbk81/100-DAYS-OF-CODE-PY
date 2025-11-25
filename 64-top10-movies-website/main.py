056630b637680b80e2576c923b8d620a2486e635
MBP <dev@tbk81.com>
2025-11-24 16:46:46 -0800

    d64 movie list

--- 64-top10-movies-website/main.py
+++ 64-top10-movies-website/main.py
@@ -48,42 +48,40 @@ with app.app_context():
 # new_movie = Movie(
 #     title="Phone Booth",
 #     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an
-#     extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with
-#     the caller leads to a jaw-dropping climax.",
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an"
+#     "extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with"
+#     "the caller leads to a jaw-dropping climax.",
 #     rating=7.3,
 #     ranking=10,
 #     review="My favourite character was the caller.",
 #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
 # )
 
-# with app.app_context():
-#     db.session.add(new_movie)
-#     db.session.commit()
-
 # second_movie = Movie(
 #     title="Avatar The Way of Water",
 #     year=2022,
-#     description="Set more than a decade after the events of the first film, learn the story of the
-#     Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they
-#     go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#     description="Set more than a decade after the events of the first film, learn the story of the"
+#     "Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they"
+#     "go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
 #     rating=7.3,
 #     ranking=9,
 #     review="I liked the water.",
 #     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
 # )
-
+#
 # with app.app_context():
+#     db.session.add(new_movie)
 #     db.session.add(second_movie)
 #     db.session.commit()
 
 @app.route("/")
 def home():
     # Construct a query to select from the database. Returns the rows in the database
-    result = db.session.execute(db.select(Movie).order_by(Movie.title))
+    result = db.session.execute(db.select(Movie))
     # Use .scalars() to get the elements rather than entire rows from the database
     all_movies = result.scalars().all()
     return render_template("index.html", all_movies=all_movies)
+
 
 
 if __name__ == '__main__':
