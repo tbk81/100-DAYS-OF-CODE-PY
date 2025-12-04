98f1ab9e16e9ef1572c9e481fcdbfbd3a302bc14
MBP <dev@tbk81.com>
2025-12-04 11:12:35 -0800

    d64 movie list

--- 64-top10-movies-website/main.py
+++ 64-top10-movies-website/main.py
@@ -85,6 +85,11 @@ def home():
     result = db.session.execute(db.select(Movie))
     # Use .scalars() to get the elements rather than entire rows from the database
     all_movies = result.scalars().all()
+
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
+
     return render_template("index.html", all_movies=all_movies)
 
 
@@ -108,6 +113,26 @@ def delete_movie():
     db.session.delete(movie)
     db.session.commit()
     return redirect(url_for("home"))
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+
+        # Redirect to /edit route
+        return redirect(url_for("rate_movie", id=new_movie.id))
 
 
 if __name__ == '__main__':
