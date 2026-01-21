87b18b949db9d1d9f7b60cfa00afbdb6b35f058e
MBP <dev@tbk81.com>
2026-01-20 16:26:59 -0800

    d82 text to morse code

--- 82-text-to-morse-code/text-morse-code.py
+++ 82-text-to-morse-code/text-morse-code.py
@@ -1,3 +1,5 @@
+from art import logo
+
 MORSE_CODE_DICT = {
     'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',
     'E': '.',     'F': '..-.',  'G': '--.',   'H': '....',
@@ -17,46 +19,26 @@ MORSE_CODE_DICT = {
     '(': '-.--.',  ')': '-.--.-'
 }
 
-app_on = True
-while app_on:
-    usr_input = input("Enter your text to translate to Morse Code: ")
+def morse_code_translate(text):
     output = []
-    if usr_input == "exit":
-        app_on = False
-    else:
-        for char in usr_input.upper():
-            if char == " ":
-                output.append(" ")
-            else:
-                output.append(MORSE_CODE_DICT[char])
-        print(output)
+    for char in text.upper():
+        if char == " ":
+            output.append(" ")
+        else:
+            output.append(MORSE_CODE_DICT[char])
+    return ' '.join(output)
+
+def main():
+    print(logo)
+    app_on = True
+    while app_on:
+        usr_input = input("Enter your text to translate to Morse Code: ")
+        if usr_input == "exit":
+            app_on = False
+        else:
+            result = morse_code_translate(usr_input)
+            print(result)
 
 
-# if __name__ == "__main__":
-#
-#     main()
-
-
-
-
-
-# def text_to_morse(text: str) -> str:
-#     morse_output = []
-#
-#     for char in text.upper():
-#         if char == ' ':
-#             morse_output.append('/')
-#         elif char in MORSE_CODE_DICT:
-#             morse_output.append(MORSE_CODE_DICT[char])
-#         else:
-#             pass
-#
-#     return ' '.join(morse_output)
-#
-#
-# def main():
-#     print("=== TEXT TO MORSE CODE ===")
-#     user_input = input("Enter your text: ")
-#     morse_result = text_to_morse(user_input)
-#     print("\nMorse Code:")
-#     print(morse_result)
+if __name__ == "__main__":
+    main()
